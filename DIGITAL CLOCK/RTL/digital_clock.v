7f906662453a113f4dce89deb73e5d4387faf87d
Chirag Dalsaniya <161462820+Chirag0302@users.noreply.github.com>
2026-01-13 19:17:28 +0530

    Add files via upload

---
+++ DIGITAL CLOCK/RTL/digital_clock.v
@@ -0,0 +1,30 @@
+module digital_clock(clk, reset, second, minute, hour);
+
+	input clk,reset;
+	output reg [2:0]second;
+	output reg [2:0]minute;
+	output reg [2:0]hour;
+
+always@(posedge clk)
+	begin
+		if (reset)begin
+			second=0;
+			minute=0;
+			hour=0;
+		end
+		else
+			second=second+1;
+			if (second==6) begin
+				 second=0;
+				 minute=minute+1;
+					if (minute==6)begin
+						 minute=0;
+						 hour=hour+1;
+							if (hour==6)begin
+								 hour=0;
+						   end
+					end
+			end
+	end
+endmodule
+
---
+++ DIGITAL CLOCK/TB/digital_clock_tb.v
@@ -0,0 +1,36 @@
+module digital_clock_tb();
+
+reg reset;
+reg clk;
+wire [2:0]second;
+wire [2:0]minute;
+wire [2:0]hour;
+
+
+//instantiation
+
+digital_clock DUT(.reset(reset),.clk(clk),.second(second),.minute(minute),.hour(hour));
+
+
+//clock generation
+
+`timescale 1s/1ms
+
+initial begin
+
+clk=1'b0;
+forever
+#5 clk=~clk;  //1hz frequency mate
+end
+
+
+initial begin
+
+reset=1;
+#10;
+reset=0;
+
+end
+
+
+endmodule
